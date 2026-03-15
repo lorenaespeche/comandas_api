88214505399d1d8bd2cfe6315edcf96ae0081228
Lorena <lorenamesquitaespeche@gmail.com>
2026-03-15 00:51:49 -0300

    Finalizando estrutura inicial

--- src/routers/ClienteRouter.py
+++ src/routers/ClienteRouter.py
@@ -8,21 +8,21 @@ router = APIRouter()
 # criar as rotas/endpoints: GET, POST, PUT, DELETE
 
 @router.get("/cliente/", tags=["Cliente"], status_code=200)
-def get_cliente():
+async def get_cliente():
     return {"msg": "cliente get todos executado"}
 
 @router.get("/cliente/{id}", tags=["Cliente"], status_code=200)
-def get_cliente(id: int):
+async def get_cliente(id: int):
     return {"msg": "cliente get um executado"}
 
 @router.post("/cliente/", tags=["Cliente"], status_code=200)
-def post_cliente(corpo: Cliente):
+async def post_cliente(corpo: Cliente):
     return {"msg": "cliente post executado", "nome": corpo.nome, "cpf": corpo.cpf, "telefone": corpo.telefone}
 
 @router.put("/cliente/{id}", tags=["Cliente"], status_code=200)
-def put_cliente(id: int, corpo: Cliente):
+async def put_cliente(id: int, corpo: Cliente):
     return {"msg": "cliente put executado", "id":id, "nome": corpo.nome, "cpf": corpo.cpf, "telefone": corpo.telefone}
 
 @router.delete("/cliente/{id}", tags=["Cliente"], status_code=200)
-def delete_cliente(id: int):
+async def delete_cliente(id: int):
     return {"msg": "cliente delete executado", "id":id}
--- src/routers/FuncionarioRouter.py
+++ src/routers/FuncionarioRouter.py
@@ -8,21 +8,21 @@ router = APIRouter()
 # criar as rotas/endpoints: GET, POST, PUT, DELETE
 
 @router.get("/funcionario/", tags=["Funcionário"], status_code=200)
-def get_funcionario():
+async def get_funcionario():
     return {"msg": "funcionario get todos executado"}
 
 @router.get("/funcionario/{id}", tags=["Funcionário"], status_code=200)
-def get_funcionario(id: int):
+async def get_funcionario(id: int):
     return {"msg": "funcionario get um executado"}
 
 @router.post("/funcionario/", tags=["Funcionário"], status_code=200)
-def post_funcionario(corpo: Funcionario):
+async def post_funcionario(corpo: Funcionario):
     return {"msg": "funcionario post executado", "nome": corpo.nome, "cpf": corpo.cpf, "telefone": corpo.telefone}
 
 @router.put("/funcionario/{id}", tags=["Funcionário"], status_code=200)
-def put_funcionario(id: int, corpo: Funcionario):
+async def put_funcionario(id: int, corpo: Funcionario):
     return {"msg": "funcionario put executado", "id":id, "nome": corpo.nome, "cpf": corpo.cpf, "telefone": corpo.telefone}
 
 @router.delete("/funcionario/{id}", tags=["Funcionário"], status_code=200)
-def delete_funcionario(id: int):
+async def delete_funcionario(id: int):
     return {"msg": "funcionario delete executado", "id":id}
